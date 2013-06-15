242c7de4eaa5527921bbf47038d92a034be99b93
christianrauh <christianrauh@ubuntu>
2013-06-14 22:22:17 -0400

    initial commit

---
+++ InKommand.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   InKommand.cpp
+ * Author: christianrauh
+ *
+ * Created on June 14, 2013, 9:49 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "hello world!! wow" << endl;
+    return 0;
+}
+
